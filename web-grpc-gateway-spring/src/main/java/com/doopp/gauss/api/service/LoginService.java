00014b19d4e474be7e85580ff48a481961a00f9f
yi.liu <liuyi@doopp.com>
2018-08-21 19:31:24 +0800

    11

--- web-grpc-gateway-spring/src/main/java/com/doopp/gauss/api/service/LoginService.java
+++ web-grpc-gateway-spring/src/main/java/com/doopp/gauss/api/service/LoginService.java
@@ -5,4 +5,10 @@ import com.doopp.gauss.common.entity.User;
 public interface LoginService {
 
     User tokenUser(String sessionToken);
+
+    User login(String account, String password);
+
+    User register(String account, String password);
+
+    User logout(String account, String password);
 }
--- web-grpc-gateway-spring/src/main/java/com/doopp/gauss/api/service/impl/LoginServiceImpl.java
+++ web-grpc-gateway-spring/src/main/java/com/doopp/gauss/api/service/impl/LoginServiceImpl.java
@@ -2,14 +2,29 @@ package com.doopp.gauss.api.service.impl;
 
 import com.doopp.gauss.api.service.LoginService;
 import com.doopp.gauss.common.entity.User;
+import io.grpc.internal.GrpcUtil;
 import org.springframework.stereotype.Service;
 
-
-@Service("accountService")
+@Service("loginService")
 public class LoginServiceImpl implements LoginService {
 
     @Override
     public User tokenUser(String sessionToken) {
+        return null;
+    }
+
+    @Override
+    public User login(String account, String password) {
+        return null;
+    }
+
+    @Override
+    public User register(String account, String password) {
+        return null;
+    }
+
+    @Override
+    public User logout(String account, String password) {
         return null;
     }
 }
