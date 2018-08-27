51a642e75dea9907fb7caaa1e15915c045807f47
koocyton <koocyton@gmail.com>
2018-08-28 00:26:48 +0800

    hello

--- web-grpc-gateway-guice/src/main/java/com/doopp/gauss/api/service/impl/AccountServiceImpl.java
+++ web-grpc-gateway-guice/src/main/java/com/doopp/gauss/api/service/impl/AccountServiceImpl.java
@@ -3,13 +3,19 @@ package com.doopp.gauss.api.service.impl;
 import com.doopp.gauss.api.service.AccountService;
 import com.doopp.gauss.common.entity.User;
 import com.doopp.gauss.common.exception.GaussException;
+import com.doopp.gauss.common.grpc.UserGrpcClient;
 import com.doopp.gauss.common.util.IdWorker;
 import com.google.inject.Inject;
+import io.grpc.gauss.user.LoginReply;
 
 public class AccountServiceImpl implements AccountService {
 
+    @Inject
+    private UserGrpcClient userGrpcClient;
+
     @Override
     public String userLogin(String account, String password) {
+        LoginReply reply = userGrpcClient.login(account, password);
         return "userLogin";
     }
 
